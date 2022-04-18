ab1d38bdf284af612a5512f380ed38836a01d232
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2022-04-18 10:46:21 +0200

    Create models according to the provided ERD diagram

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :manage, Payment, author: user
+    can :manage, Expense, author: user
     can :manage, Group, user: user
   end
 end
