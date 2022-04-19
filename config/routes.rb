1668192e9d3e908176e23be7b93d3a795c5e6e55
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2022-04-19 12:03:33 +0200

    Create transactions page and categories page, and their forms respectively

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   unauthenticated :user do
     root to: 'users#index'
   end
+
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
+  resources :groups
+  resources :expenses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_18_090017) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_18_141643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount", precision: 10, scale: 2
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
+
+  create_table "expenses_groups", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "expense_id", null: false
+    t.index ["group_id", "expense_id"], name: "index_expenses_groups_on_group_id_and_expense_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_18_090017) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "users", column: "author_id"
+  add_foreign_key "groups", "users"
 end
